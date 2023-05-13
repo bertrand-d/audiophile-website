46e263a265541cccd70959fb474333337d872e45
reuby-dev <delphineb57@gmail.com>
2023-05-13 21:22:21 +0200

    add hero section and buttons

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,13 @@
 import './css/main.scss'
 import Footer from './layouts/Footer';
 import Header from './layouts/Header'
+import Homepage from './pages/Homepage';
 
 export default function App() {
   return (
     <div className="App">
       <Header/>
+      <Homepage/>
       <Footer/>
     </div>
   );
---
+++ front/src/components/Hero.js
@@ -0,0 +1,23 @@
+export default function Hero() {
+    return (
+        <section className="section-hero">
+            <div className="section-hero-content">
+                <span className="section-hero-new">New product</span>
+                <h1 className="section-hero-title">
+                    <span className="section-hero-product-name">
+                        XX99 mark II
+                    </span>
+                    <span className="section-hero-category-name">
+                        Headphones
+                    </span>
+                </h1>
+                <p className="section-hero-description">
+                    Experience natural, lifelike audio and exceptional
+                    build quality made for the passionate music
+                    enthusiast.
+                </p>
+                <a href="#" className="button-primary">See product</a>
+            </div>
+        </section>
+    )
+}

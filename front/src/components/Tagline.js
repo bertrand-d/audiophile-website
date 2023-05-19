809cba7eaade2491cd8046d932553ca990490f67
reuby-dev <delphineb57@gmail.com>
2023-05-19 16:59:56 +0200

    add tagline section

--- front/src/components/Tagline.js
+++ front/src/components/Tagline.js
@@ -1,6 +1,23 @@
-export default function Tagline(){
-    return(
+export default function Tagline() {
+    return (
         <section className="section-tagline max-content">
+            <div className="section-tagline-left">
+                <h2 className="medium-title">
+                    Bringing you the
+                    <span className="tertiary-color"> best </span>
+                    audio gear
+                </h2>
+                <p>
+                    Located at the heart of New York City, Audiophile is the premier store for high end
+                    headphones, earphones, speakers, and audio accessories. We have a large showroom and
+                    luxury demonstration rooms available for you to browse and experience a wide range
+                    of our products. Stop by our store to meet some of the fantastic people who make
+                    Audiophile the best place to buy your portable audio equipment.
+                </p>
+            </div>
+            <div className="section-tagline-right">
+                <img src ="../assets/shared/desktop/image-best-gear.jpg"/>
+            </div>
         </section>
     )
 }

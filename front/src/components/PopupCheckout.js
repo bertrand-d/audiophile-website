23b1ce58e53ca6e41370d46342c172f938206a46
reuby-dev <delphineb57@gmail.com>
2023-11-09 13:22:16 +0100

    add presentation to checkout popup

--- front/src/components/PopupCheckout.js
+++ front/src/components/PopupCheckout.js
@@ -1,13 +1,16 @@
+import { ReactComponent as CheckedLogo } from '../assets/icons/checked.svg'
+import { Link } from 'react-router-dom'
+
 export default function PopupCheckout({ show, onClose }) {
 
     return (
         <div className={`popup-container ${show ? 'show' : 'hide'}`}>
             <div className="cart-box cart-box-centered cart-box-checkout ">
-                <div>logo checkout</div>
-                <span>THANK YOU FOR YOUR ORDER</span>
+                <CheckedLogo/>
+                <span className="small-title">THANK YOU<br/> FOR YOUR ORDER</span>
                 <span>You will receive an email confirmation shorty</span>
                 <div>Contenu du panier et total</div>
-                <button className="button-primary" onClick={onClose}>Back to home</button>
+                <Link to="/" className="button-primary">Back to home</Link>
             </div>
         </div>
     )

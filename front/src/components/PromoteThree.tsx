c929df440116d3a1a21bee93c4804a246b1f3166
reuby-dev <delphineb57@gmail.com>
2024-03-14 14:04:26 +0100

    add promote three img

--- front/src/components/PromoteThree.tsx
+++ front/src/components/PromoteThree.tsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'
+import promoteImg from '../assets/home/desktop/image-earphones-yx1.jpg'
 
 export default function PromoteThree() {
     return (
         <div className="promote-three-content">
             <div className="promote-three-picture-container">
-                <img className="promote-three-picture" src="\assets\home\desktop\image-earphones-yx1.jpg" alt="yx1" />
+                <img className="promote-three-picture" src={promoteImg} alt="yx1" />
             </div>
             <div className="promote-three-content-text">
                 <h2 className="promote-three-title small-title">

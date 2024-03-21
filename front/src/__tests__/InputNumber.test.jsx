a20ea5d98f3cd2da1f61ae78832bc3c048766b44
reuby-dev <delphineb57@gmail.com>
2024-03-21 09:34:55 +0100

    test

--- front/src/__tests__/InputNumber.test.jsx
+++ front/src/__tests__/InputNumber.test.jsx
@@ -1,12 +1,20 @@
-import {inputNumber} from "../components/InputNumber"
+import {InputNumber} from "../components/InputNumber"
 import { fireEvent, render, screen } from "@testing-library/react";
 
 test("increase input number on click on +", () => {
-  const increaseMock = jest.fn();
-  render(<inputNumber onUpdate={increaseMock} />);
-  expect(screen.queryByTestId("checkIcon")).not.toBeInTheDocument(); // check that the icon is not rendered
+  let inputQuantity = 0
+  const setInputQuantity = (qty) => {
+    inputQuantity = qty;
+  }
+  render(<InputNumber quantity={inputQuantity} callback={setInputQuantity} />)
+  expect(screen.queryByTestId("checkIcon")).not.toBeInTheDocument() //je dois tester l'affichage (les éléments, bouto, - + input etc)
   const btn = screen.getByRole("button"); // get the button (pressable)
   fireEvent.click(btn); // click it
+
+  // expect(inputQuantity).toBe(1)
+
+  // click bouton -
+  // expect(inputquantity).toBe(0)
 
   expect(screen.getByTestId("checkIcon")).toBeInTheDocument(); // check that the icon is displayed
   expect(increaseMock).toHaveBeenCalledTimes(1); // make sure that the onUpdate function that was passed via props was clicked
